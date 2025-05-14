a1c6a5d523a0267ea85308f629d46658416d56f3
changbeom87 <parkchangbeom87@gmail.com>
2025-05-14 16:15:07 +0900

    djWJrn

--- link.js
+++ link.js
@@ -1,3 +1,6 @@
 const div = document.getElementById("emalil");
 
-div.addEventListener("click", () => (navigator.clipboard.writeText("parkchangbeom87@gmail.com");));
+div.addEventListener("click", () => {
+    navigator.clipboard.writeText("parkchangbeom87@gmail.com");
+alert("이메일이 복사되었습니다.");
+});
